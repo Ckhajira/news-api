94bb79788fbd1675cefca41474afd5a5c0e38949
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-14 22:37:57 +0300

    This commit adds the getters and setters

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -13,4 +13,36 @@ public class User{
         this.title = title;
         this.role = role;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
