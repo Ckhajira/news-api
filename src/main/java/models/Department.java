a23ad8566377857ddb0d9983fe4d3e53bbda280f
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-14 22:21:50 +0300

    This commit adds the department class with variables

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.Objects;
+
+public class Department{
+    private int id;
+    private String name;
+    private String description;
+    private String size;
+}
