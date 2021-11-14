f7713024356a6b316864c7bae20f6fa1ca58dec9
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-14 22:31:12 +0300

    This commit adds the equals() and hashCode to enable comparison of instances

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -46,4 +46,21 @@ public class Department{
         this.size = size;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(size, that.size);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, size);
+    }
+
+
 }
