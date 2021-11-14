83c05638e8334e9b31dfa88fdd4fb7e4306331b5
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-14 23:06:15 +0300

    This commit adds a constructor to the Depnews

--- src/main/java/models/Depnews.java
+++ src/main/java/models/Depnews.java
@@ -12,4 +12,13 @@ public class Depnews implements Comparable<Depnews>{
     private int departmentId;
     private long createdat;
     private String formattedCreatedAt;
+
+    public Depnews(String content, String writtenBy, int rating, int departmentId){
+        this.content = content;
+        this.writtenBy = writtenBy;
+        this.rating = rating;
+        this.departmentId = departmentId;
+        this.createdat = System.currentTimeMillis();
+        setFormattedCreatedAt();
+    }
 }
