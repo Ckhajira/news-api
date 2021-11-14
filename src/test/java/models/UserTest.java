e1ef223872c96daa41824a6d743c48d007a5d1b3
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-14 22:40:10 +0300

    This commit adds the tests for the User

---
+++ src/test/java/models/UserTest.java
@@ -0,0 +1,48 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getTitle() {
+    }
+
+    @Test
+    public void setTitle() {
+    }
+
+    @Test
+    public void getRole() {
+    }
+
+    @Test
+    public void setRole() {
+    }
+}
