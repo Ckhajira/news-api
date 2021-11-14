1f0c615c3df50ddfac50d3e7e63bf66da25593c4
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-14 23:58:17 +0300

    This commit adds the logic to the add method od the Department Dao

--- src/main/java/models/dao/Sql2oDepartmentDao.java
+++ src/main/java/models/dao/Sql2oDepartmentDao.java
@@ -8,3 +8,25 @@ import org.sql2o.Sql2oException;
 
 import java.util.ArrayList;
 import java.util.List;
+
+public class Sql2oDepartmentDao implements DepartmentDao{
+    private final Sql2o sql2o;
+    public Sql2oDepartmentDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Department department) {
+        String sql = "INSERT INTO departments (name, description, size) VALUES (:name, :description, :size)";
+        try(Connection con = sql2o.open()){
+            int id = (int)con.createQuery(sql, true)
+                    .bind(department)
+                    .executeUpdate()
+                    .getKey();
+            department.setId(id);
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+}
