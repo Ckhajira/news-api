242d25e4e7c4e8aef7e5357a0fc47bf2aece0a06
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-14 22:26:16 +0300

    This commit adds the getters and the setters

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -13,4 +13,37 @@ public class Department{
         this.description = description;
         this.size =size;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
 }
