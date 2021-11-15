975fa2e3890b5c4a7ff754d4239b2996512a6eb4
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-15 10:45:11 +0300

    This commit adds the imports to the Sql2oUserDao

---
+++ src/main/java/models/dao/Sql2oUserDao.java
@@ -0,0 +1,10 @@
+package models.dao;
+
+import models.Department;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.ArrayList;
+import java.util.List;
