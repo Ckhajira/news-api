8f961fbbce0a6ff2a6d01631e5784a9e7a5e11db
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-15 13:03:41 +0300

    This commit imports the necessary files to app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,3 +14,22 @@ import org.sql2o.Sql2o;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+public class App{
+    public static void main(String[] args){
+        Sql2oUserDao userDao;
+        Sql2oDepartmentDao departmentDao;
+        Sql2oDepnewsDao depnewsDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        staticFileLocation("/public");
+        String connectionString ="jdbc:h2:~/APIorganisation.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "","");
+
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        userDao = new Sql2oUserDao(sql2o);
+        depnewsDao = new Sql2oDepnewsDao(sql2o);
+
+    }
+}
